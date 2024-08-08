a5b09f1625ca5e41051d8eb9656a469d8d109a1b
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 10:27:57 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Get started!
+let name = prompt("what is your name")
+
+console.log("welcome to my game," + name)
+
+
+let color = prompt("pick what color you want to be?(red,blue,green,yellow,pink,purple")
+
+
+console.log(`Okay, ${name}, your role is now ${color}.`)
+
+let mix = prompt("What color do you want to mix level?")
+
+let color2 = ("pick what color you want to be?(red,blue,green,yellow,pink,purple")
+
+console.log("okay thats a great choice!")
+
+const rainbow = "funfact there are only 7 colors in the rainbow;"
